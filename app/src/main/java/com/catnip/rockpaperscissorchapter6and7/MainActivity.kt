70781defd593d9e550272d842369ad85302dc23b
yafizh <a.chairil.yafizh@gmail.com>
2021-10-20 20:16:40 +0800

    Feature Splash Screen with asset

--- app/src/main/java/com/catnip/rockpaperscissorchapter6and7/MainActivity.kt
+++ app/src/main/java/com/catnip/rockpaperscissorchapter6and7/MainActivity.kt
@@ -4,11 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.catnip.rockpaperscissorchapter6and7.ui.intro.IntroActivity
+import com.catnip.rockpaperscissorchapter6and7.ui.splashscreen.SplashScreenActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        startActivity(Intent(this, IntroActivity::class.java))
+//        startActivity(Intent(this, SplashScreenActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,33 @@
+package com.catnip.rockpaperscissorchapter6and7.ui.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import com.catnip.rockpaperscissorchapter6and7.R
+import com.catnip.rockpaperscissorchapter6and7.ui.intro.IntroActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var timer : CountDownTimer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        setSplash()
+    }
+
+    private fun setSplash() {
+        timer = object : CountDownTimer(3000,1000){
+            override fun onTick(p0: Long) {            }
+
+            override fun onFinish() {
+                val intent = Intent(this@SplashScreenActivity, IntroActivity::class.java)
+                startActivity(intent)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                finish()
+            }
+        }
+        timer?.start()
+    }
+}
