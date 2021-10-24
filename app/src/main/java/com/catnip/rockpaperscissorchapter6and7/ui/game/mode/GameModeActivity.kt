2c278d457a61207a606a9110550f38784a8520db
yafizh <a.chairil.yafizh@gmail.com>
2021-10-24 12:10:29 +0800

    Fix menus name issue

--- app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/game/mode/GameModeActivity.kt
+++ app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/game/mode/GameModeActivity.kt
@@ -1,6 +1,8 @@
 package com.catnip.rockpaperscissorchapter6and7.ui.game.mode
 
+import com.catnip.rockpaperscissorchapter6and7.R
 import com.catnip.rockpaperscissorchapter6and7.base.BaseActivity
+import com.catnip.rockpaperscissorchapter6and7.data.local.preference.UserPreference
 import com.catnip.rockpaperscissorchapter6and7.databinding.ActivityGameModeBinding
 import com.catnip.rockpaperscissorchapter6and7.ui.game.mode.dialog.PlayerMenusDialogFragment
 
@@ -12,7 +14,15 @@ class GameModeActivity : BaseActivity<ActivityGameModeBinding, GameModeContract.
 
     override fun initView() {
         supportActionBar?.hide()
+        setPlayerName()
         setClickListeners()
+    }
+
+    private fun setPlayerName() {
+        getViewBinding().tvPlayerNamePvp.text =
+            getString(R.string.text_format_player_name, UserPreference(this).username)
+        getViewBinding().tvPlayerNamePvr.text =
+            getString(R.string.text_format_player_name, UserPreference(this).username)
     }
 
     override fun initPresenter() {
