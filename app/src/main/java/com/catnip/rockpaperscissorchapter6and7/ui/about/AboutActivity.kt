e5d9b458973522df7ede2ce0af56e3250bfb85df
Louis Aldorio <louisaldorio@gmail.com>
2021-11-13 13:49:27 +0700

    Video tutorial dialog to view model

--- app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/about/AboutActivity.kt
+++ app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/about/AboutActivity.kt
@@ -1,27 +1,17 @@
 package com.catnip.rockpaperscissorchapter6and7.ui.about
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.catnip.rockpaperscissorchapter6and7.base.*
 import com.catnip.rockpaperscissorchapter6and7.base.model.Resource
-import com.catnip.rockpaperscissorchapter6and7.data.local.room.PlayersDatabase
 import com.catnip.rockpaperscissorchapter6and7.data.local.room.datasource.AboutDataSourceImpl
-import com.catnip.rockpaperscissorchapter6and7.data.local.room.datasource.PlayersDataSourceImpl
 import com.catnip.rockpaperscissorchapter6and7.data.model.TeamMember
 import com.catnip.rockpaperscissorchapter6and7.databinding.ActivityAboutBinding
-import com.catnip.rockpaperscissorchapter6and7.databinding.ActivityMenuBinding
-import com.catnip.rockpaperscissorchapter6and7.databinding.DialogFragmentVideoTutorialBinding
 import com.catnip.rockpaperscissorchapter6and7.ui.about.adapter.TeamMemberRecyclerViewAdapter
-import com.catnip.rockpaperscissorchapter6and7.ui.game.MenuContract
-import com.catnip.rockpaperscissorchapter6and7.ui.game.mode.dialog.PlayerMenusPresenter
-import com.catnip.rockpaperscissorchapter6and7.ui.game.mode.dialog.PlayerMenusRepository
-import com.catnip.rockpaperscissorchapter6and7.ui.tutorial.VideoTutorialContract
 
 class AboutActivity : BaseViewModelActivity<ActivityAboutBinding>(
     ActivityAboutBinding::inflate
-), AboutViewModelContract.View {
+), AboutContract.View {
 
     private lateinit var viewModel: AboutViewModel
 
