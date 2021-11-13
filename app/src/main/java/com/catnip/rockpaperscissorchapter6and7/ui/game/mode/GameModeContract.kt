7c4669a87fc843e9569b2c492ec1d6c579c2da6b
paijolongor <zidankarim5@gmail.com>
2021-11-13 12:28:51 +0700

    change game mode name

--- app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/game/mode/GameModeContract.kt
+++ app/src/main/java/com/catnip/rockpaperscissorchapter6and7/ui/game/mode/GameModeContract.kt
@@ -1,14 +1,14 @@
 package com.catnip.rockpaperscissorchapter6and7.ui.game.mode
 
-import com.catnip.rockpaperscissorchapter6and7.base.BaseContract
+import com.catnip.rockpaperscissorchapter6and7.base.BaseViewModelContract
 
 interface GameModeContract {
-    interface View : BaseContract.BaseView {
+    interface View : BaseViewModelContract.BaseView {
         fun setClickListeners()
         fun showDialogChoosePlayer()
     }
 
-    interface Presenter : BaseContract.BasePresenter {
+    interface ViewModel {
 
     }
 }
